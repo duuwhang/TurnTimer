04dda1a454d1d3cceed63e36dfb4fecb242bc3e7
Maxi Pulstorm <maxipulstorm@aon.at>
2020-10-05 17:14:19 +0200

    format MainActivity

--- app/src/main/java/com/turntimer/MainActivity.java
+++ app/src/main/java/com/turntimer/MainActivity.java
@@ -3,35 +3,32 @@ package com.turntimer;
 import android.content.res.Resources;
 import android.os.Bundle;
 import android.view.WindowManager;
-import android.annotation.SuppressLint;
 import androidx.appcompat.app.AppCompatActivity;
-
+import java.util.Objects;
 import com.turntimer.layouts.MainLayout;
 
-import java.util.Objects;
-
-@SuppressLint("StaticFieldLeak")
 public class MainActivity extends AppCompatActivity
 {
-    private static MainActivity m;
     private MainLayout mainLayout;
+    private static MainActivity m;
     public static DisplayMetricsController displayMetricsController;
-
-    public MainActivity() {
+    
+    public MainActivity()
+    {
         m = this;
     }
-
+    
     public static MainActivity getInstance()
     {
         return m;
     }
-
+    
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         Objects.requireNonNull(this.getSupportActionBar()).hide();
-
+        
         Resources resources = getResources();
         WindowManager windowManager = getWindowManager();
         displayMetricsController = new DisplayMetricsController(windowManager.getDefaultDisplay(), resources.getDisplayMetrics().density);
