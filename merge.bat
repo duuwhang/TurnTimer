f9092d76a412c03b2b9cdf043786420bf758b0cc
duu whang <43827670+duuwhang@users.noreply.github.com>
2020-12-23 11:20:58 +0100

    update merge.bat

--- merge.bat
+++ merge.bat
@@ -1,16 +1,14 @@
-@echo off
-ECHO SOURCE BRANCH IS %BUILD_SOURCEBRANCH%
-ECHO GIT CHECKOUT MAIN
+
 git checkout main
-ECHO GIT STATUS
+
 git status
-ECHO GIT ADD
+
 git add .
-ECHO GIT STATUS
+
 git status
-ECHO GIT commit
+
 git commit -m "CD commit"
-ECHO GIT PUSH
+
 git push origin
-ECHO GIT STATUS
+
 git status
