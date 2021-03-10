246b42b25adb8612c03431faf377dd0bd66f39a0
Maxi Pulstorm <maxipulstorm@aon.at>
2021-03-10 11:10:21 +0100

    initialize android tests

---
+++ app/src/test/java/com/turntimer/MainActivityTests.java
@@ -0,0 +1,37 @@
+package com.turntimer;
+
+import org.junit.Test;
+
+public class MainActivityTests
+{
+    @Test
+    public void getInstance_shouldReturnValidInstance()
+    {
+    }
+    
+    @Test
+    public void onCreate()
+    {
+    }
+    
+    @Test
+    public void getSaveStateOption()
+    {
+    }
+    
+    @Test
+    public void setSaveStateOption()
+    {
+    
+    }
+    
+    @Test
+    public void getLoading()
+    {
+    }
+    
+    @Test
+    public void getLayout()
+    {
+    }
+}
