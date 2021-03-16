cd9984f47cc5ac0d1f50f409b885589cad7f274c
Maxi Pulstorm <maxipulstorm@aon.at>
2021-03-16 13:17:49 +0100

    test tests

--- app/src/test/java/com/turntimer/DisplayMetricsControllerTests.java
+++ app/src/test/java/com/turntimer/DisplayMetricsControllerTests.java
@@ -1,9 +1,14 @@
 package com.turntimer;
 
+import android.util.DisplayMetrics;
 import android.view.Display;
 import org.junit.Assert;
 import org.junit.Test;
 import org.mockito.Mock;
+import org.mockito.invocation.InvocationOnMock;
+import org.mockito.stubbing.Answer;
+import static org.mockito.Matchers.any;
+import static org.mockito.Mockito.when;
 
 public class DisplayMetricsControllerTests
 {
@@ -22,9 +27,41 @@ public class DisplayMetricsControllerTests
         Assert.assertEquals(360, displayMetricsController.dpToPx(dp));
     }
     
+    //nested class
+    private static Answer<Void> reverseMsg() {
+        return new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) {
+                return null;
+            }
+        };
+    }
+    @Mock
+    DisplayMetrics displayMetricsMock = new DisplayMetrics();
     @Test
     public void getScreenHeight()
     {
+        int height = 1000;
+        DisplayMetricsController displayMetricsController = new DisplayMetricsController(displayMock, 120);
+        displayMetricsMock.heightPixels = height;/*
+        doAnswer(new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) {
+                DisplayMetrics displayMetrics = (DisplayMetrics) invocation.getArguments()[0];
+                displayMetrics.setTo(displayMetricsMock);
+                return null;
+            }
+        }).when(displayMock).getMetrics(any(DisplayMetrics.class));
+        */
+        //when(displayMock.getMetrics(any(DisplayMetrics.class))).then(reverseMsg());
+    
+        when(displayMock.getMetrics(any(DisplayMetrics.class))).thenAnswer(new Answer<Void>() {
+            public Void answer(InvocationOnMock invocation) {
+            DisplayMetrics displayMetrics = (DisplayMetrics) invocation.getArguments()[0];
+            displayMetrics.setTo(displayMetricsMock);
+            return null;
+        }});
+        
+        
+        Assert.assertEquals(height, displayMetricsController.getScreenHeight());
     }
     
     @Test
