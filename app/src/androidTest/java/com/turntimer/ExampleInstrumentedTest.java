30a8bec06e44efbc9598b16bb48a444b0a58cad2
duuwhang <maxipulstorm@aon.at>
2021-07-01 17:40:46 +0200

    use AndroidJUnit4ClassRunner

--- app/src/androidTest/java/com/turntimer/ExampleInstrumentedTest.java
+++ app/src/androidTest/java/com/turntimer/ExampleInstrumentedTest.java
@@ -1,13 +1,13 @@
 package com.turntimer;
 
 import android.content.Context;
-import androidx.test.ext.junit.runners.AndroidJUnit4;
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner;
 import androidx.test.platform.app.InstrumentationRegistry;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import static org.junit.Assert.assertEquals;
 
-@RunWith(AndroidJUnit4.class)
+@RunWith(AndroidJUnit4ClassRunner.class)
 public class ExampleInstrumentedTest
 {
     @Test
